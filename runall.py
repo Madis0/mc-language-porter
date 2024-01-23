06acb01b0957ab9f9b57c6583c3aba96f2f4774a
Madis <Madis0@users.noreply.github.com>
2024-01-23 18:12:15 +0100

    Extra steps as variable

--- runall.py
+++ runall.py
@@ -2,6 +2,7 @@ import subprocess
 translationLang = "et_EE"
 useDownloader = True
 usePackager = False
+extraSteps = True
 
 if useDownloader:
     print("Executing the downloader script...")
@@ -10,11 +11,16 @@ if useDownloader:
 
 print("Now executing the mapper script...")
 subprocess.call(["python", "mapper.py"])
-print("Mapper script completed.\n\nNow executing the spawn egg adding script...")
-subprocess.call(["python", "spawn-egg-adder.py"])
-print("Spawn egg adding script completed.\n\nNow executing the extra mapping cleanup...")
-subprocess.call(["python", "clean-extra-mappings.py"])
-print("Extra mapping cleanup completed.\n\nNow executing the porting script...")
+print("Mapper script completed.")
+
+if extraSteps:
+    print("\nNow executing the spawn egg adding script...")
+    subprocess.call(["python", "spawn-egg-adder.py"])
+    print("Spawn egg adding script completed.\n\nNow executing the extra mapping cleanup...")
+    subprocess.call(["python", "clean-extra-mappings.py"])
+    print("\nExtra mapping cleanup completed.")
+
+print("Now executing the porting script...")
 subprocess.call(["python", "porter.py", translationLang])
 print("Porting script completed.")
 
